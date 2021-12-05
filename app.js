e6d84d00b1837a66e6092ce9535a63898296b85d
wesley-bruno-cocreare <91733387+wesley-bruno-cocreare@users.noreply.github.com>
2021-12-05 12:59:49 -0300

    alter api

--- app.js
+++ app.js
@@ -1,58 +1,50 @@
-const express = require('express');
-const mongoose = require('mongoose');
-var cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+var cors = require("cors");
 
-
-const ToDoSchema = require('./schemas/ToDoSchema');
+const ProductSchema = require("./schemas/ProductSchema");
 
 const server = express();
 
-server.use(cors())
+server.use(cors());
 
 server.use(express.json());
 
-mongoose.connect('mongodb+srv://........mongodb.net/todo_list?retryWrites=true&w=majority',
+mongoose.connect(
+  "mongodb+srv://admin:254565@cluster0.pdzrk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   {
     useUnifiedTopology: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
   }
 );
 
-server.get('/', (req, res) => {
-  return res.json({ message: 'Seja bem vindo à API do TODO_LIST' });
-})
-
-server.post('/todo', async (req, res) => {
-  const { title, date } = req.body;
-  if (!title || !date) {
-    return res.status(400).json({ message: 'Validations Fails' })
-  }
-  const todo = await ToDoSchema.create(req.body)
-  return res.status(201).json(todo);
+server.get("/", (req, res) => {
+  return res.json({ message: "Seja bem vindo à API" });
 });
 
-server.get('/todo', async (req, res) => {
-  const todos = await ToDoSchema.find();
-  return res.json(todos);
+server.get("/products", async (req, res) => {
+  const products = await ProductSchema.find();
+  return res.json(products);
 });
 
-server.get('/todo/:id', async (req, res) => {
+server.get("/products/:id", async (req, res) => {
   const { id } = req.params;
-  const todo = await ToDoSchema.findById(id);
-  return res.json(todo);
+  const product = await ProductSchema.findById(id);
+  return res.json(product);
 });
 
-server.put('/todo/:id', async (req, res) => {
-  const { id } = req.params;
-  const todo = await ToDoSchema.findOneAndUpdate({ '_id': id }, req.body);
-  return res.json(todo);
-})
+server.post("/products", async (req, res) => {
+  const { name, price, promotionalPrice, image } = req.body;
 
-server.delete('/todo/:id', async (req, res) => {
-  const { id } = req.params;
-  const todo = await ToDoSchema.deleteOne({ '_id': id });
-  return res.json({ message: 'Successfully deleted' });
-})
+  const product = await ProductSchema.create({
+    name,
+    price,
+    promotionalPrice,
+    image,
+  });
+  return res.json(product);
+});
 
-
-server.listen(3333, () => console.log('Servidor iniciado em http://localhost:3333'))
+server.listen(3333, () =>
+  console.log("Servidor iniciado em http://localhost:3333")
+);
